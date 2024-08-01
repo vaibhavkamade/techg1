817995ea902e72bc42fbe0f380e6aa5db080a20d
vaibhav kamade <vaibhavkamade20@gmail.com>
2024-08-01 11:44:03 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://techg1-2.vercel.app/",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
 };
